160ff943758218b7e7a252372335c2314446d0ad
Muhammet Işık <isikmuhamm@gmail.com>
2024-11-01 16:17:55 +0300

    Simulation duration control added to top of code as a global constant. Now simulation prints time step and initial states as it choosed randomly.

--- reinforcement-learning/pendulum_zero_simulation.py
+++ reinforcement-learning/pendulum_zero_simulation.py
@@ -3,13 +3,15 @@ from scipy.integrate import odeint
 import matplotlib.pyplot as plt
 from pendulum_nonlinear_model import PendulumEnvironment
 
+SIMULATION_DURATION = 400
+
 if __name__ == "__main__":
     # Örnek kullanım:
     env = PendulumEnvironment()
 
     # Başlangıç durumu tanımlamaları
-    initial_state = [0.0, 0.0, np.random.uniform(-np.pi/18, np.pi/18), 0.0]
-    duration = 400
+    duration = SIMULATION_DURATION
+    initial_state = [0.0, 0.0, np.random.uniform(-np.pi/18, +np.pi/18), 0.0]
     time_series = int(duration/env.time_step)
     force = np.zeros(time_series)   
     states = np.zeros((time_series, 4))
@@ -21,6 +23,6 @@ if __name__ == "__main__":
     
     # Sonuçları kaydet
     save_folder = "reinforcement-learning"
-    #np.save(f"{save_folder}/states_zero.npy", states)
+    np.save(f"{save_folder}/states_zero.npy", states)
     
-    print(f"Simülasyon kuvvet uygulanmaksızın tamamlandı. Sonuçlar kaydedildi. Zaman adımı {env.time_step} saniyedir.")
+    print(f"Simülasyon kuvvet uygulanmaksızın tamamlandı. Sonuçlar kaydedildi. Zaman adımı {env.time_step} saniyedir. Başlangıç koşulu {initial_state} şeklindedir.")
